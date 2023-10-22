7ce510399734e904ad0855ca91c9bdcb09b8570f
emma <223729@ids.upchiapas.edu.mx>
2023-10-22 13:00:19 -0600

    Actualizacion de consultas

--- src/controllers/clientes.controller.js
+++ src/controllers/clientes.controller.js
@@ -1,33 +1,40 @@
 const db = require('../configs/db.configs');
 
 
-const getAdmin = (req, res) => {
-    db.query('SELECT * FROM administrador', (err, results) => {
-        if (err) {
-            console.log("Error al optener los datos del administrador", err);
-            res.status(500).json({ error: "Error al optener al administrador" });
+const getAdmin = async(req, res) => {
+    try {
+        const results = await new Promise((resolve, reject) => {
+            db.query('SELECT * FROM administrador', (err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+        res.json({ message: 'Datos del administrador', data: results });
+    } catch (error) {
+        res.status(500).json({
+            message: 'ocurrio un error al optener los datos',
+            error: error.message
+        });
+    }
+};
 
-        } else {
-            res.json({ message: 'datos del administrador:', data: results });
-        }
-    });
+
+const getProduct = async(req, res) => {
+    try {
+        const results = await new Promise((resolve, reject) => {
+            db.query('SELECT productos.*, categoria.categoria AS categoria FROM productos INNER JOIN categoria ON productos.categoria_id_categoria = categoria.id_categoria', (err, results) => {
+                if (err) reject(err);
+                else(resolve)(results);
+            });
+        });
+        res.json({ message: 'Datos de  los productos :', data: results });
+    } catch (error) {
+        return res.status(500).json({
+            message: 'ocurrio un error al obtener los productos',
+            error: error.message
+        });
+    }
 }
-
-
-const getProduct = (req, res) => {
-    db.query('SELECT productos.*, categoria.categoria AS categoria FROM productos INNER JOIN categoria ON productos.categoria_id_categoria = categoria.id_categoria', (err, results) => {
-        if (err) {
-            console.log("Error al optener los productos", err);
-            res.status(500).json({ error: "Error al optener los productos" });
-
-        } else {
-            res.json({ message: 'Productos:', data: results });
-        }
-    });
-}
-
-
-
 
 
 module.exports = {
