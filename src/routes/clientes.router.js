0cacb4e67e96a4fb4edf1cd8fd3afd4272eecdd3
emma <223729@ids.upchiapas.edu.mx>
2023-11-10 23:17:41 -0600

    se agrego el api de payemnets

--- src/routes/clientes.router.js
+++ src/routes/clientes.router.js
@@ -6,10 +6,11 @@ const usuariosController = require('../controllers/clientes.controller');
 
 router.get('/productos', usuariosController.getProduct);
 router.post('/crear-clientes', usuariosController.CreateClient);
-router.post('/pagos', usuariosController.payments);
+router.post('/pagos', usuariosController.pagos);
 router.get('/clientes/:id', usuariosController.getCliente);
 router.put('/clientes/:id', usuariosController.updateubicacion);
 router.post('/pedidos', usuariosController.CreatePedido);
+
 
 
 module.exports = router;
